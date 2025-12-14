cd59f11e77124bc086ba5f07c83b46520ea05233
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2025-12-14 18:02:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-//your JS code here. If required.
-const fileNames = [
-  "file1.txt",
-  "file2.txt",
-  "file3.txt",
-  "file4.txt",
-  "file5.txt",
-];
 
 // do not change the code above
 // add your code here
+// Given array of file names
+const files = [
+  'file1.txt',
+  'file2.txt',
+  'file3.txt',
+  'file4.txt',
+  'file5.txt'
+];
+
+// Read and log all files asynchronously
+async function readFilesAsync() {
+  try {
+    // Create fetch promises for all files
+    const promises = files.map(file =>
+      fetch(file).then(response => response.text())
+    );
+
+    // Wait for all files to be read
+    const contents = await Promise.all(promises);
+
+    // Log each file content
+    contents.forEach(content => {
+      console.log(content);
+    });
+
+  } catch (error) {
+    console.error('Error reading files:', error);
+  }
+}
+
+// Execute function
+readFilesAsync();
